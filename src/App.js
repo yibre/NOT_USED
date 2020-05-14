e4c9f48a805f06b13683fd3ad0915ea10b9cb6e2
yibre <cdoyue@gmail.com>
2020-05-14 20:11:13 +0900

    fixing main page now
    css error need to be fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './homeSwipe';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="ACNH_Menu">
+        <logo></logo>
+        <text>ALL ABOUT ACNH</text>
       </header>
+      <p>AD</p>
+      <div className = "Test">
+        <div className = "Menu_left"></div>
+        <div className = "Menu_right">
+          <div className="gallery js-flickity"
+            data-flickity-options='{ "wrapAround": true }'>
+            <div class="gallery-cell"></div>
+            <div class="gallery-cell"></div>
+            <div class="gallery-cell"></div>
+            <div class="gallery-cell"></div>
+            <div class="gallery-cell"></div>
+          </div>
+          <div className = "Status"></div>
+        </div>
+      </div>
     </div>
   );
 }
